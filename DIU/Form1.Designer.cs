ad89ee1b8b14e7c030277648c11784927cddc7e1
Shahriar Aziz Aakash <aakashaayat@gmail.com>
2019-04-12 20:20:27 +0600

    Initial Commit
    Initial Commit

---
+++ DIU/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace DIU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sid_tb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.name_tb = new System.Windows.Forms.TextBox();
+            this.email_tb = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.save_btn = new System.Windows.Forms.Button();
+            this.greet_btn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.update_btn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.delete_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sid_tb
+            // 
+            this.sid_tb.Location = new System.Drawing.Point(221, 37);
+            this.sid_tb.Name = "sid_tb";
+            this.sid_tb.Size = new System.Drawing.Size(164, 22);
+            this.sid_tb.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Student ID:";
+            // 
+            // name_tb
+            // 
+            this.name_tb.Location = new System.Drawing.Point(221, 80);
+            this.name_tb.Name = "name_tb";
+            this.name_tb.Size = new System.Drawing.Size(164, 22);
+            this.name_tb.TabIndex = 2;
+            // 
+            // email_tb
+            // 
+            this.email_tb.Location = new System.Drawing.Point(221, 125);
+            this.email_tb.Name = "email_tb";
+            this.email_tb.Size = new System.Drawing.Size(164, 22);
+            this.email_tb.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(158, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Email:";
+            // 
+            // save_btn
+            // 
+            this.save_btn.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.save_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.save_btn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.save_btn.Location = new System.Drawing.Point(212, 171);
+            this.save_btn.Name = "save_btn";
+            this.save_btn.Size = new System.Drawing.Size(173, 42);
+            this.save_btn.TabIndex = 12;
+            this.save_btn.Text = "Save";
+            this.save_btn.UseVisualStyleBackColor = false;
+            this.save_btn.Click += new System.EventHandler(this.save_btn_Click);
+            // 
+            // greet_btn
+            // 
+            this.greet_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.greet_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greet_btn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.greet_btn.Location = new System.Drawing.Point(212, 232);
+            this.greet_btn.Name = "greet_btn";
+            this.greet_btn.Size = new System.Drawing.Size(173, 42);
+            this.greet_btn.TabIndex = 13;
+            this.greet_btn.Text = "Greet!";
+            this.greet_btn.UseVisualStyleBackColor = false;
+            this.greet_btn.Click += new System.EventHandler(this.greet_btn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(516, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(777, 426);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // update_btn
+            // 
+            this.update_btn.BackColor = System.Drawing.Color.Yellow;
+            this.update_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.update_btn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.update_btn.Location = new System.Drawing.Point(212, 293);
+            this.update_btn.Name = "update_btn";
+            this.update_btn.Size = new System.Drawing.Size(173, 39);
+            this.update_btn.TabIndex = 15;
+            this.update_btn.Text = "Update";
+            this.update_btn.UseVisualStyleBackColor = false;
+            this.update_btn.Click += new System.EventHandler(this.update_btn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Font = new System.Drawing.Font("Pristina", 20F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label3.Location = new System.Drawing.Point(43, 394);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(260, 45);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Register with pleasure!";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // delete_btn
+            // 
+            this.delete_btn.BackColor = System.Drawing.Color.Red;
+            this.delete_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delete_btn.ForeColor = System.Drawing.Color.Black;
+            this.delete_btn.Location = new System.Drawing.Point(212, 351);
+            this.delete_btn.Name = "delete_btn";
+            this.delete_btn.Size = new System.Drawing.Size(173, 40);
+            this.delete_btn.TabIndex = 17;
+            this.delete_btn.Text = "Delete";
+            this.delete_btn.UseVisualStyleBackColor = false;
+            this.delete_btn.Click += new System.EventHandler(this.delete_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1305, 450);
+            this.Controls.Add(this.delete_btn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.update_btn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.greet_btn);
+            this.Controls.Add(this.save_btn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.email_tb);
+            this.Controls.Add(this.name_tb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sid_tb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sid_tb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox name_tb;
+        private System.Windows.Forms.TextBox email_tb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button save_btn;
+        private System.Windows.Forms.Button greet_btn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button update_btn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button delete_btn;
+    }
+}
+
---
+++ DIU/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace DIU
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection connection = new MySqlConnection();
+        private string connectionString = "server=localhost;user id=root;persistsecurityinfo=True;database=crud;password=1110188516aA*";
+        private MySqlCommand cmd;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void greet_btn_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Hello");
+            MessageBox.Show("Hello " + name_tb.Text);
+        }
+
+        public void retrive()
+        {
+            connection.ConnectionString = connectionString;
+            cmd = connection.CreateCommand();
+
+            try
+            {
+
+                string query = "select * from crudtable";
+
+
+                cmd.CommandText = query;
+                connection.Open();
+                MySqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+
+                dt.Load(reader);
+
+                dataGridView1.DataSource = dt;
+
+                reader.Close();
+
+
+            }
+            catch (Exception e1)
+            {
+                string msg = e1.Message.ToString();
+                string caption = "Error";
+                MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cmd.Dispose();
+                connection.Close();
+            }
+        }
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            retrive();
+        }
+        
+
+        private void save_btn_Click(object sender, EventArgs e)
+        {
+            {
+
+                string id = sid_tb.Text;
+                string name = name_tb.Text;
+                string email = email_tb.Text;
+
+                if ((id == "") || (name == "") || (email == ""))
+                {
+                    string msg = "No textbox can be empty";
+                    string caption = "Error";
+                    MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    connection.ConnectionString = connectionString;
+                    cmd = connection.CreateCommand();
+
+                    try
+                    {
+
+                        string query = "Insert into crudtable values('"
+                            + id + "','"
+                            + name + "','"
+                            + email + "')";
+
+
+                        cmd.CommandText = query;
+                        connection.Open();
+                        cmd.ExecuteScalar();
+
+
+                    }
+                    catch (Exception e1)
+                    {
+                        string msg = e1.Message.ToString();
+                        string caption = "Error";
+                        MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        cmd.Dispose();
+                        connection.Close();
+                        retrive();
+                    }
+                }
+            }
+        }
+
+        public void updateName()
+        {
+            connection.ConnectionString = connectionString;
+            cmd = connection.CreateCommand();
+
+            try
+            {
+
+                string query = "UPDATE crudtable SET \"name\" = '"+name_tb.Text+"' WHERE \"sid\" = "+sid_tb.Text;
+
+
+                cmd.CommandText = query;
+                connection.Open();
+                cmd.ExecuteScalar();
+
+
+            }
+            catch (Exception e1)
+            {
+                string msg = e1.Message.ToString();
+                string caption = "Error";
+                MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cmd.Dispose();
+                connection.Close();
+                retrive();
+            }
+        }
+
+        public void updateEmail()
+        {
+            connection.ConnectionString = connectionString;
+            cmd = connection.CreateCommand();
+
+            try
+            {
+
+                string query = "UPDATE crudtable SET \"email\" = '" + email_tb.Text + "' WHERE \"sid\" = " + sid_tb.Text;
+                
+                cmd.CommandText = query;
+                connection.Open();
+                cmd.ExecuteScalar();
+
+
+            }
+            catch (Exception e1)
+            {
+                string msg = e1.Message.ToString();
+                string caption = "Error";
+                MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cmd.Dispose();
+                connection.Close();
+                retrive();
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            connection.ConnectionString = connectionString;
+            cmd = connection.CreateCommand();
+
+            try
+            {
+                string query = "select * from crudtable";
+                cmd.CommandText = query;
+                connection.Open();
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+
+                dataGridView1.DataSource = dt;
+
+                reader.Close();
+
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message.ToString();
+                string caption = "Error";
+                MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cmd.Dispose();
+                connection.Close();
+            }
+        }
+
+        private void update_btn_Click(object sender, EventArgs e)
+        {
+            if (sid_tb.Text != "")
+            {
+                if (name_tb.Text != "")
+                {
+                    updateName();
+                    MessageBox.Show("Name Change Successful!");
+                }
+                else
+                {
+                    MessageBox.Show("Name field is empty! Name was not changed!");
+                }
+
+                if (email_tb.Text != "")
+                {
+                    updateEmail();
+                    MessageBox.Show("Email Change Successful!");
+                }
+                else
+                {
+                    MessageBox.Show("Email field is empty! Email was not changed!");
+                }
+            }
+            else
+            {
+               MessageBox.Show("Student ID Can't be empty!");
+            }
+        }
+
+        private void delete_btn_Click(object sender, EventArgs e)
+        {
+            if (sid_tb.Text != "")
+            {
+                connection.ConnectionString = connectionString;
+                cmd = connection.CreateCommand();
+
+                try
+                {
+
+                    string query = "DELETE FROM crudtable WHERE sid = " + sid_tb.Text;
+
+                    cmd.CommandText = query;
+                    connection.Open();
+                    cmd.ExecuteScalar();
+
+
+                }
+                catch (Exception e1)
+                {
+                    string msg = e1.Message.ToString();
+                    string caption = "Error";
+                    MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    cmd.Dispose();
+                    connection.Close();
+                    retrive();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Could not delete! Student ID was empty!");
+            }
+            
+        }
+    }
+}
